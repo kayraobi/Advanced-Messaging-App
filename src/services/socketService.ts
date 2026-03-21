fe35c48bf3a4bb2047bbfe816d7382b146cd9636
Bahadır Karakuş <bahadirkarakus261@gmail.com>
2026-03-21 01:48:45 +0100

    Add socket service client wrapper

---
+++ src/services/socketService.ts
@@ -0,0 +1,26 @@
+import { io, Socket } from 'socket.io-client';
+
+const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL ?? 'http://localhost:3030';
+
+class SocketService {
+  private socket: Socket | null = null;
+
+  connect(): Socket {
+    if (!this.socket) {
+      this.socket = io(SOCKET_URL, {
+        transports: ['websocket'],
+      });
+    }
+
+    return this.socket;
+  }
+
+  disconnect(): void {
+    if (this.socket) {
+      this.socket.disconnect();
+      this.socket = null;
+    }
+  }
+}
+
+export const socketService = new SocketService();
